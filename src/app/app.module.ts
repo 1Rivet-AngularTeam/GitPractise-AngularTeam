56d658071b43f76ee89f44f11a876ba2e90f889f
Aastha Yadav <aastha.yadav@1rivet.local>
2020-02-27 16:58:55 +0530

    Aastha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AasthaComponent } from './aastha/aastha.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AasthaComponent
   ],
   imports: [
     BrowserModule,
