37390ba9f56bcf510d4102be82deac30470f096c
Amit Chauhan <amit.chauhan@1rivet.local>
2020-02-27 16:34:29 +0530

    add: amit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AmitChauhanComponent } from './amit-chauhan/amit-chauhan.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AmitChauhanComponent
   ],
   imports: [
     BrowserModule,
