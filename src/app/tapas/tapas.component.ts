917ac4f8040f497286e5649eda1dffe5b441410c
Tapas Vashi <tapas.vashi@1rivet.local>
2020-02-27 17:30:03 +0530

    Add: Added component

---
+++ src/app/tapas/tapas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tapas',
+  templateUrl: './tapas.component.html',
+  styleUrls: ['./tapas.component.scss']
+})
+export class TapasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
