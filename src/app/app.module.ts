39ad163bf09e74164de312b7eb0a728e8c3846dd
Shiv Desai <shiv.desai@1rivet.local>
2020-02-27 16:45:38 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShivComponentComponent } from './shiv-component/shiv-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShivComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shiv-component/shiv-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shiv-component',
+  templateUrl: './shiv-component.component.html',
+  styleUrls: ['./shiv-component.component.scss']
+})
+export class ShivComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
